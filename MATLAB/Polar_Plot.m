3e531073552135f61f10f290a2ad1aad6bb41d3c
IssacJay <issacjt1999@gmail.com>
2022-06-23 22:47:53 +0100

    V0.2
    Converting 2D Polar Pattern to 3D Polar Pattern

--- MATLAB/Polar_Plot.m
+++ MATLAB/Polar_Plot.m
@@ -1,6 +1,7 @@
 clear all
 close all
-polarpatern = zeros(360,1);
+
+%Polar Pattern Generator
 %Polar pattern = a + b(cos(theta))
 % 1 0 omni-directional
 % 0.75 0.25 sub-cardioid 
@@ -8,17 +9,28 @@ polarpatern = zeros(360,1);
 % 0.25 0.75 hyper-cardioid 
 % 0 1 figure-8 (bi-directional)
 
-a = 0;
-b = 1;
+%Input Variables
+a = 0.75; %Constant
+b = 0.25; %Coefficient
+aa = 0;
+bb = 1;
+%Output Variables 
+polpat = zeros(360,3); %dB output array
 
-for i = 1:360
-    g = a + (b*(cos(deg2rad(i))));
-    polarpatern(i,1) = 10 * log10(abs(g));
+%Calculate Magnitude Responce for each Angle
+for i = 1:360 %For all Azimuthal Angles
+    g = (a + (b*(cos(deg2rad(i))))) + (aa + (bb*(cos(deg2rad(i))))); %Calculate Magnitude Responce
+    polpat(i,1) = 10 * log10(abs(g)); %Convert from Mag to dB
+    polpat(i, 2) = i; %Azimuthal Angle
 end
-polarpatern(polarpatern < -30) = -30; 
+
+%Convert to Cartesian
+polpat(polpat < -30) = -30; %Limit responce to -30dB 
+%[X,Y] = pol2cart(polpat(:,2),polpat(:,2)); %Cartesian
+
 
 axes = polaraxes;
-h = polarplot(axes, polarpatern);
+h = polarplot(axes, polpat);
 axes.ThetaZeroLocation = 'top';
 rlim([-30 0])
 rticks([-24,-18,-12,-6])
@@ -26,12 +38,17 @@ rticklabels(["-24dB","-18dB","-12dB","-6dB","0dB"])
 title("Cardioid Polar Pattern")
 subtitleString = strcat('a =  ', num2str(a), ', b =  ', num2str(b));
 subtitle(subtitleString)
-set(h,'LineWidth',3); %sets the 'LineWidth' property of the plotted line only
+set(h,'LineWidth',2); %sets the 'LineWidth' property of the plotted line only
 
 %3D Plot
 % theta = [1;1;360];
-% phi = polarpatern;
+% phi = polpat;
 % rho = 1;
 % [theta,phi] = meshgrid(theta,phi);
 % [x,y,z] = sph2cart(theta,phi,rho);
 % surf(x,y,z)
+% 
+% r=sqrt(x^2+y^2)
+% theta=arctan(y/x)
+% 
+% sqrt(x^2+y^2) = A + Bcos(arctan(y/x))

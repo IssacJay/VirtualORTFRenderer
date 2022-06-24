a79855b6e27d3753a111f1292b7c1e373d16b0e8
IssacJay <issacjt1999@gmail.com>
2022-06-24 09:26:13 +0100

    V0.2
    Polar Plot represented in cartesian coordinates, but still not in 3D

--- MATLAB/Polar_Plot.m
+++ MATLAB/Polar_Plot.m
@@ -10,25 +10,36 @@ close all
 % 0 1 figure-8 (bi-directional)
 
 %Input Variables
-a = 0.75; %Constant
-b = 0.25; %Coefficient
-aa = 0;
-bb = 1;
-%Output Variables 
-polpat = zeros(360,3); %dB output array
+a = 0.5; %Constant
+b = 0.5; %Coefficient
 
+%Output Variables 
+polpat = zeros(36,3); %dB output array
+cart = zeros(36,3);
 %Calculate Magnitude Responce for each Angle
-for i = 1:360 %For all Azimuthal Angles
-    g = (a + (b*(cos(deg2rad(i))))) + (aa + (bb*(cos(deg2rad(i))))); %Calculate Magnitude Responce
+for i = 1:36 %For all Azimuthal Angles
+    theta = i * 10 - 10;
+    if theta == 0
+        theta = 360;
+    end
+    g = (a + (b*(cos(deg2rad(theta))))); %Calculate Magnitude Responce
     polpat(i,1) = 10 * log10(abs(g)); %Convert from Mag to dB
-    polpat(i, 2) = i; %Azimuthal Angle
+    polpat(i, 2) = theta; %Azimuthal Angle
+    
+
+    %Cartesian
+    phi = 90; 
+    cart(i,1) = g * sind(phi) * cosd(theta); 
+    cart(i,2) = g * sind(phi) * sind(theta);
+    cart(i,3) = g * cosd(phi); 
 end
+
 
 %Convert to Cartesian
 polpat(polpat < -30) = -30; %Limit responce to -30dB 
-%[X,Y] = pol2cart(polpat(:,2),polpat(:,2)); %Cartesian
 
-
+%Plot 2D Polar Pattern
+figure(1)
 axes = polaraxes;
 h = polarplot(axes, polpat);
 axes.ThetaZeroLocation = 'top';
@@ -40,15 +51,6 @@ subtitleString = strcat('a =  ', num2str(a), ', b =  ', num2str(b));
 subtitle(subtitleString)
 set(h,'LineWidth',2); %sets the 'LineWidth' property of the plotted line only
 
-%3D Plot
-% theta = [1;1;360];
-% phi = polpat;
-% rho = 1;
-% [theta,phi] = meshgrid(theta,phi);
-% [x,y,z] = sph2cart(theta,phi,rho);
-% surf(x,y,z)
-% 
-% r=sqrt(x^2+y^2)
-% theta=arctan(y/x)
-% 
-% sqrt(x^2+y^2) = A + Bcos(arctan(y/x))
+%Plot 3D Polar Pattern
+figure(2)
+plot(cart(:,1),cart(:,2))

7535479f465ff68933581efc0a861141fdc66c7f
IssacJay <issacjt1999@gmail.com>
2022-07-07 22:43:20 +0100

    V0.2
    Currently working in test bench, but not working as VST

--- MATLAB/VST/VirtualMicrophone.m
+++ MATLAB/VST/VirtualMicrophone.m
@@ -1,4 +1,4 @@
-classdef VirtualMicrophone < audioPlugin                            % <== (1) Inherit from audioPlugin.
+classdef VirtualMicrophone < audioPlugin                          
     properties
         %Microphone Array Properties
         X_Spacing = 0; 
@@ -9,13 +9,14 @@ classdef VirtualMicrophone < audioPlugin                            % <== (1) In
         Elevation = 0; 
         Gain = 1;
     end
-    properties (Access = private)
+    properties
         bool = false;
         Resp = zeros(4,4);
-        MicArray = phased.ConformalArray();
+        MicArray = phased.ConformalArray;
     end
+
     properties (Constant)
-        PluginInterface = audioPluginInterface( ...           % <== (3) Map tunable property to plugin parameter.
+        PluginInterface = audioPluginInterface( ...         
             audioPluginParameter('Y_Spacing', 'DisplayName','Mic Y Spacing', ...
                 'Mapping',{'int',0, 50}),...
             audioPluginParameter('X_Spacing', 'DisplayName', 'Mic X Spacing', ...
@@ -30,13 +31,8 @@ classdef VirtualMicrophone < audioPlugin                            % <== (1) In
                 'Mapping',{'lin',0,1}));
     end  
     methods
-        function plugin = VirtualMicrophone()
-        end
-        function out = process(plugin, in)   
-            if plugin.bool == false
-                 plugin.MicArray = MicrophoneArray(plugin.X_Spacing,plugin.Y_Spacing,plugin.MicAngle, plugin.MicArray);
-                 plugin.bool = true; 
-            end
+        function out = process(plugin, in) 
+            plugin.MicArray = MicrophoneArray(plugin.X_Spacing,plugin.Y_Spacing,plugin.MicAngle, plugin.MicArray);
             plugin.Resp = plugin.MicArray(2000, [plugin.Azimuth; plugin.Elevation]); 
             in(:,1) = (in(:,1) + in(:,2))/2; %Convert to Mono
             in(:,2) = in(:,1);
@@ -44,9 +40,11 @@ classdef VirtualMicrophone < audioPlugin                            % <== (1) In
             r = in(:,2) * db2mag((mag2db(plugin.Resp(3,1)) + mag2db(plugin.Resp(4,1)))); 
             out = [l, r];
         end
+        function reset(plugin)
+        end
         function set.X_Spacing(plugin, val)
             plugin.X_Spacing= val;
-            %plugin.MicArray = MicrophoneArray(val,plugin.Y_Spacing,plugin.MicAngle, plugin.MicArray);
+            %plugin.MicArray = MicrophoneArray(val,plugin.Y_Spacing,plugin.MicAngle);
         end
     end
 

303a019544eefe4bfc5d741491b46710cac0579b
IssacJay <62423825+IssacJay@users.noreply.github.com>
2022-09-08 11:41:12 +0100

    V0.8

--- MATLAB/IRSet/ORTF_CueCalc.m
+++ MATLAB/IRSet/ORTF_CueCalc.m
@@ -1,14 +1,44 @@
-%% ORTF ITD and ILD Calculator
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@IssacJay
+IssacJay
+/
+VirtualORTFRenderer
+Public
+Code
+Issues
+Pull requests
+1
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+VirtualORTFRenderer/MATLAB/ORTF_CueCalc.m
+@IssacJay
+IssacJay V0.5
+Latest commit ebf0972 on 7 Jul
+ History
+ 1 contributor
+171 lines (141 sloc)  4.85 KB
+
+%ORTF ITD and ILD Calculator
 clear all
 close all
 %Source Variables
-Theta = 0; %Source Direction (angle of incidence) in degrees
+Theta = 90; %Source Direction (angle of incidence) in degrees
 
 %Microphone Variables
-MicrophoneSpacing = 0.2; %Microphone Spacing in Meters 
-MicrophoneAngle = 100; %Microphone Angle in degrees
+MicrophoneSpacing = 0.17; %Microphone Spacing in Meters
+MicrophoneAngle = 110; %Microphone Angle in degrees
 
-aa = 0.5;
+a = 0.5;
 b = 0.5;
 
 %Binaural Variables
@@ -21,14 +51,14 @@ a = MicrophoneAngle; %Assing to new variable
 
 
 MicValues = zeros(360, 6);
-BinauralValues = zeros(360, 5); 
+BinauralValues = zeros(360, 5);
 PhantomSourceShift = zeros(360,2);
 for n = -180:179
-    Theta = n; 
+    Theta = n;
     index = n + 181;
     
     %Calculate ICTD and ICLD of the Microphone
-    MicValues(index, 1) = n; 
+    MicValues(index, 1) = n;
     MicValues(index, 2) = sind(Theta) * (m/c); %Calculate Interchannel Time Difference
       
     if Theta == 180 - a/2
@@ -37,8 +67,8 @@ for n = -180:179
         Theta = -179 + a/2;
     end
     
-    MicValues(index, 3) = mag2db((0.5 + 0.5*cosd(Theta - a/2))/(0.5 + 0.5*cosd(Theta + a/2))); %Calculate Interchannel Level Difference 
-    MicValues(index, 3) = mag2db(aa + b*tand(Theta - a/2));
+    MicValues(index, 3) = mag2db((0.5 + 0.5*cosd(Theta - a/2))/(0.5 + 0.5*cosd(Theta + a/2))); %Calculate Interchannel Level Difference
+    MicValues(index, 3) = mag2db(a + b*tand(Theta - a/2));
     MicValues(index, 4 ) = mag2db((1 + cosd(Theta + a/2))); %Left Microphone
     MicValues(index, 5) = mag2db((1 + cosd(Theta - a/2))); %Right Microphone
    
@@ -46,12 +76,12 @@ for n = -180:179
     if  MicValues(index, 3) < -100
          MicValues(index, 3) = -100;
          MicValues(index, 4) = -100;
-         MicValues(index, 5) = -100; 
+         MicValues(index, 5) = -100;
     end
     if  MicValues(index, 3) > 100
          MicValues(index, 3) = 100;
          MicValues(index, 4) = 100;
-         MicValues(index, 5) = 100; 
+         MicValues(index, 5) = 100;
     end
 
     %Calculate ITD and ILD for Binaural
@@ -63,23 +93,23 @@ for n = -180:179
     end
     Theta = deg2rad(Theta);
     Rho = deg2rad(Rho);
-    HeadRadius = HeadWidth/2; 
+    HeadRadius = HeadWidth/2;
     ITD = ((HeadRadius*(Rho)) + (HeadRadius * sin(Rho)))/c; %Calculate Interchannel Time Difference
-    ILD = HeadWidth * sqrt(1) * sin(Theta); %Calculate Interchannel Level Difference 
+    ILD = HeadWidth * sqrt(1) * sin(Theta); %Calculate Interchannel Level Difference
     ILD_dB = log10(ILD) * 20; %Calculate Interchannel Level Difference in dB
     ILD_dB = 20 *sin(Theta);
     
    
 
     BinauralValues(index, 1) = n;
-    BinauralValues(index, 2) = ITD; 
+    BinauralValues(index, 2) = ITD;
     BinauralValues(index, 3) = ILD_dB;
     BinauralValues(index, 4) = ILD_dB;
 
     %Phantom Source Shift
     ITDShift = MicValues(index,2) * 13;
-    ILDShift = MicValues(index, 3) * 0.075; 
-    PhantomSourceShift(index, 1) = ITDShift + ILDShift; 
+    ILDShift = MicValues(index, 3) * 0.075;
+    PhantomSourceShift(index, 1) = ITDShift + ILDShift;
     
 end
 subplot(2,1,1)
@@ -89,7 +119,7 @@ hold on
 plot(MicValues(:,1),BinauralValues(:,2), 'r')
 xlabel('Source Angle (°)')
 ylabel('ICTD (Sec)')
-xlim([-90 90])
+xlim([-180 180])
 xticks(-180:30:180);
 legend('ORTF', 'Binaural', 'Location', 'SouthEast')
 
@@ -101,7 +131,86 @@ hold on
 plot(MicValues(:,1),BinauralValues(:,4), 'r');
 xlabel('Source Angle (°)')
 ylabel('ICLD (dB)')
-xlim([-90 90])
+xlim([-180 180])
 xticks(-180:30:180);
 %ylim([-85 85])
 legend('ORTF', 'Binaural', 'Location', 'SouthEast')
+
+
+
+
+
+%Apply Cues to Signal
+Angle = -40;
+Fs = 44100;
+[Audio, fs] = audioread('Clarinet.wav');
+Audio = resample(Audio, Fs, fs);
+Audio = Audio(:,1);
+SampLength = length(Audio);
+ORTFAudio = zeros(SampLength,2);
+
+Index=find(MicValues==Angle,1);
+delay = round((MicValues(Index,2)*44100));
+
+%Apply ICTD
+if delay > 0
+    ORTFAudio(delay:end, 1) = Audio(1:end-delay + 1, 1);
+    ORTFAudio(:,2) = Audio;
+elseif delay < 0
+    delay = abs(delay);
+    ORTFAudio(:,1) = Audio(:,1);
+    ORTFAudio(delay:end, 2) = Audio(1:end-delay + 1, 1);
+elseif delay == 0
+     ORTFAudio(:,1) = Audio(:,1);
+     ORTFAudio(:,2) = Audio(:,1);
+end
+
+%Apply ICLD
+ICLD_L = db2mag(MicValues(Index, 4));
+ICLD_R = db2mag(MicValues(Index, 5));
+ORTFAudio(:,1) = ORTFAudio(:,1) .* ICLD_L; %Column 4 is Left channel dB
+ORTFAudio(:,2) = ORTFAudio(:,2) .* ICLD_R; %Column 5 is Right channel dB
+
+%Normalise
+%ORTFAudio = (0.99.* ORTFAudio./max(abs(ORTFAudio)));
+sound(ORTFAudio, Fs)
+ 
+% %Binaural Output
+BinauralAudio = zeros(SampLength, 2);
+
+Index=find(BinauralValues==Angle,1);
+binauraldelay = round((BinauralValues(Index,2)*44100));
+
+%Apply ICTD
+if binauraldelay > 0
+    BinauralAudio(binauraldelay:end, 1) = Audio(1:end-binauraldelay + 1, 1);
+    BinauralAudio(:,2) = Audio;
+elseif binauraldelay < 0
+    binauraldelay = abs(binauraldelay);
+    BinauralAudio(:,1) = Audio(:,1);
+    BinauralAudio(binauraldelay:end, 2) = Audio(1:end-binauraldelay + 1, 1);
+elseif binauraldelay == 0
+    BinauralAudio(:,1) = Audio(:,1);
+    BinauralAudio(:,2) = Audio(:,1);
+end
+%Apply ICLD
+BinauralAudio(:,1) = BinauralAudio(:,1) .* db2mag(MicValues(Index, 4)); %Column 4 is Left channel dB
+BinauralAudio(:,2) = BinauralAudio(:,2) .* db2mag(MicValues(Index, 5)); %Column 5 is Right channel dB
+ORTFAudio = 0.99.*ORTFAudio./(max(max(abs(ORTFAudio))));
+%audiowrite("Clarinet_ORTF_135.wav", ORTFAudio, Fs);
+% sound(BinauralAudio, Fs)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+You have no unread notifications
